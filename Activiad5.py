2c36e36f7cc1aff08d60d0b3fc217f98a568344d
Ale <avilalejandram@gmail.com>
2025-07-11 16:01:17 -0600

    1

---
+++ Activiad5.py
@@ -0,0 +1,6 @@
+class Estudiante:
+    def __init__(self, nombre, carne, carrera, nota_final):
+        self.nombre = nombre
+        self.carne = carne
+        self.carrera = carrera
+        self.nota_final = nota_final
